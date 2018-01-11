dfa9334034a61d5fab567040731d160109b266ac
zhaoshenghao <1213988947@qq.com>
2018-01-11 17:14:31 +0800

    first

---
+++ src/js/index/index.js
@@ -0,0 +1,24 @@
+
+    'use strict';
+
+    var mySwiper = new Swiper('.swiper-container', {
+        // direction: 'vertical',     纵向了
+        loop: true,
+    
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        
+        autoplay:true
+    })
+
+
+
+
+
+
+
+
+    
+    
